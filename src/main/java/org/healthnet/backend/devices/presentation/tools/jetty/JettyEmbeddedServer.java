c78e5123104fdc91dce70b280d9723ac8b103083
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-12-23 19:32:39 +0100

    Implemented Jetty

---
+++ src/main/java/org/healthnet/backend/devices/presentation/tools/jetty/JettyEmbeddedServer.java
@@ -0,0 +1,21 @@
+package org.healthnet.backend.devices.presentation.tools.jetty;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+
+public class JettyEmbeddedServer {
+    private final Server server;
+
+    public JettyEmbeddedServer(int port, ContextHandler contextHandler) {
+        server = new Server(port);
+        server.setHandler(contextHandler);
+    }
+
+    public void start() throws Exception {
+        server.start();
+    }
+
+    public void stop() throws Exception {
+        server.stop();
+    }
+}
