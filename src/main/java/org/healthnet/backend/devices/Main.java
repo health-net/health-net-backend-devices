992687b95bd07e067eb1606965504524f238e034
Sbyner <tmmscrltt@gmail.com>
2020-03-05 19:07:56 +0100

    [UPDATE] - Heroku stuff

--- src/main/java/org/healthnet/backend/devices/Main.java
+++ src/main/java/org/healthnet/backend/devices/Main.java
@@ -19,7 +19,7 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        MqttBroker broker = new MqttBroker("localhost", 1883);
+        MqttBroker broker = new MqttBroker(System.getenv().getOrDefault("BROKER_HOST", "localhost"), Integer.parseInt(System.getenv().getOrDefault("BROKER_PORT", "1883")));
         DeviceRepository deviceRepository = new MqttDeviceRepository(new MqttTopic("healthnet", "devices"), broker);
         Function<String, Device> singleDeviceService = new SingleDeviceService(deviceRepository);
         Function<String, List<Device>> groupDeviceByPatientService = new GroupDeviceByPatientService(deviceRepository);
@@ -29,7 +29,7 @@ public class Main {
         HttpServlet devicesServlet = new DevicesServlet(singleDeviceWebHandler, groupDeviceByPatientWebHandler, webResponseFactory);
         ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
         servletContextHandler.addServlet(new ServletHolder(devicesServlet), "/devices/*");
-        int port = Integer.parseInt(System.getenv().getOrDefault("SERVICE_PORT", "8080"));
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
         JettyEmbeddedServer server = new JettyEmbeddedServer(port, servletContextHandler);
         try {
             server.start();
